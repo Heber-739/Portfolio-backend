0692e887d571eee2a8ef70960371b8b4abe58fa0
Heber Duarte <heberduarteryr@gmail.com>
2023-03-16 17:38:49 -0300

    Implementaciòn final

---
+++ src/main/java/com/portfolio/Service/SMail.java
@@ -0,0 +1,32 @@
+package com.portfolio.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SMail {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String to;
+
+    public void sendMail(String from, String body) {
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setFrom(from);
+        mail.setTo(to);
+        mail.setSubject("Portfolio Contact");
+        mail.setText(body);
+
+        javaMailSender.send(mail);
+    }
+
+}
