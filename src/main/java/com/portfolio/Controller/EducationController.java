42290f77143accfa64f1971a4e378f3fe2c757ba
Heber Duarte <heberduarteryr@gmail.com>
2023-03-15 22:04:59 -0300

    Dockerfile2

--- src/main/java/com/portfolio/Controller/EducationController.java
+++ src/main/java/com/portfolio/Controller/EducationController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequestMapping("/education")
 public class EducationController {
